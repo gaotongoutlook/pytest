fdbcedee28ce158a09c92e93a7eb8855041cec0a
GaoTong <gao_tong@outlook.com>
2026-02-25 23:47:53 +0800

    提交练习

--- MyList.py
+++ MyList.py
@@ -1,3 +1,4 @@
+from typing import List
 from ListNode import ListNode
 
 if __name__ == '__main__':
@@ -43,8 +44,176 @@ def ReverseList(self , head: ListNode) -> ListNode:
     if not head or not head.next:
         return head
 
+    pre = None
+    cur = head
+
+    while cur:
+        next = cur.next
+        cur.next = pre
+        pre = cur
+        cur = next
+
+    return pre
+
+"""
+    链表内指定区间反转
+"""
+def reverseBetween(self , head: ListNode, m: int, n: int) -> ListNode:
+    if not head:
+        return None
+    if m==n:
+        return head
+
+    count = 1
+    pre = None
+    cur = head
+    while count<m:
+        pre = cur
+        cur = cur.next
+        count += 1
+
+
+
+    return None
+
+"""
+    合并K个已升序的链表
+"""
+def mergeKLists(self , lists: List[ListNode]) -> ListNode:
+    if not lists:
+        return None
+    if len(lists)==1:
+        return lists[0]
+    return self.merge(lists, 0, len(lists)-1)
+
+def merge(self, lists, start, end) -> ListNode:
+    if start==end:
+        return lists[start]
+
+    mid = start + (end-start)>>1
+    leftResult = self.merge(lists, start, mid)
+    rightResult = self.merge(lists, mid+1, end)
+
     dummy = ListNode(0)
-    pre = dummy
+    cur = dummy
+    while leftResult and rightResult:
+        if leftResult.val <= rightResult.val:
+            cur.next = leftResult
+            leftResult = leftResult.next
+        else:
+            cur.next = rightResult
+            rightResult = rightResult.next
+        cur = cur.next
+
+    cur.next = leftResult if leftResult else rightResult
+
+    return dummy.next
+
+"""
+    链表中的节点每k个一组翻转
+"""
+def reverseKGroup(self , head: ListNode, k: int) -> ListNode:
+
+    return None
+
+"""
+    单链表的排序 归并排序
+"""
+def sortInList(self , head: ListNode) -> ListNode:
+    if not head or not head.next:
+        return head
+
+    slow = head
+    fast = head.next # 链表的中间节点 此处要好好学习
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+
+    mid = slow.next
+    slow.next = None
+
+    left = self.sortInList(head)
+    right = self.sortInList(mid)
+
+    return self.mergeSortInList(left, right)
+
+def mergeSortInList(left: ListNode, right: ListNode) -> ListNode:
+    dummy = ListNode(0)
+    cur = dummy
+
+    while left and right:
+        if left.val <= right.val:
+            cur.next = left
+            left = left.next
+        else:
+            cur.next = right
+            right = right.next
+        cur = cur.next
+
+    cur.next = left if left else right
+
+    return dummy.next
+
+"""
+    两个链表的第一个公共节点
+"""
+def FindFirstCommonNode(self , pHead1 , pHead2 ):
+    if not pHead1 or not pHead2:
+        return None
+
+    n = 1
+    cur = pHead1
+    while cur:
+        n += 1
+        cur = cur.next
+
+    m = 1
+    cur = pHead2
+    while cur:
+        m += 1
+        cur = cur.next
+
+    cur1 = pHead1
+    cur2 = pHead2
+    if n > m:
+        while n>m:
+            n -= 1
+            cur1 = cur1.next
+    elif n < m:
+        while n<m:
+            m -= 1
+            cur2 = cur2.next
+
+    while cur1.val != cur2.val:
+        cur1 = cur1.next
+        cur2 = cur2.next
+
+    return cur1
+
+"""
+    链表中环的入口结点
+"""
+def EntryNodeOfLoop(self, pHead):
+    if not pHead:
+        return pHead
+
+    slow = pHead
+    fast = pHead
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow:
+            fast = pHead
+            while fast != slow:
+                slow = slow.next
+                fast = fast.next
+            return slow
+
+    return None
+
+
+
+
 
 
 
